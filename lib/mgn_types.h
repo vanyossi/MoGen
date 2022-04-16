732ab605712de0e45591d6a2d2e423033cc4bbab
Ivan Yossi <ghevan@gmail.com>
2022-04-16 14:13:20 -0500

    Fix add UNUSED macro and mop type

--- lib/mgn_types.h
+++ lib/mgn_types.h
@@ -4,8 +4,11 @@
 typedef struct _mgn_pop MgnPop;
 typedef enum _mgn_pop_type popType;
 
+typedef struct _mgn_mop mgnMop;
+
 typedef struct mgn_limit mgnLimit;
 
 #define checkFlag(data, flag) (data & flag) == flag
+#define UNUSED(x) ((void)(x))
 
 #endif // _MGN_TYPES_
