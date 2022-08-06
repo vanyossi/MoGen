7bba56b7b77c500e8dafdfc1dbfc1725bdf5e953
Ivan Yossi <ghevan@gmail.com>
2022-08-05 22:41:56 -0500

    update moead test parameters

--- tests/mgn_moeadtest.c
+++ tests/mgn_moeadtest.c
@@ -31,7 +31,9 @@ int main() {
 //    mgnLimit ilimit = {0, 1};
 
     mgnMop *mop = mgn_mop_alloc();
-    mop->eval = mgn_cast_eval(mgn_zdt1_vector);
+    mop->eval_array = mgn_cast_eval(mgn_zdt2);
+    strcpy(mop->name, "ZDT6");
+    mop->params = &params; // same order as mgn_indv_param
     mgnLimit *moplim = mgn_limit_alloc(params.realSize);
     for (size_t i = 0; i < moplim->size; ++i) {
         moplim->min[i] = 0;
@@ -80,22 +82,22 @@ int main() {
 //    }
 
 
-    mgnMoa *moead = mgn_moead_init(27, 2, 20, EP, mop, mgn_ind_init,moplim);
+    mgnMoa *moead = mgn_moead_init(30, 2, 20, EP, mop, mgn_ind_init,moplim);
     moead->set_ga_vals(moead,&ga_probs);
 
 //    mgn_moead_pop_init(moead,mgn_ind_init, NULL);
 
     // run must be private
-    int runs = 300;
-    int plot_every = 30;
+    int runs = 1000;
+    int plot_every = 100;
     mgn_plot_data pdat = {"", "", "f_1", "f_2",
                           -0.1f,1.1f,-0.1f,1.1f};
     strcpy(pdat.title, "MOEAD");
-    for (int run = 0; run < runs; ++run) {
+    for (int run = 1; run <= runs; ++run) {
         mgn_moa_solve(moead, 1);
 
         if (run % plot_every == 0) {
-            asprintf(&pdat.filename, "MOEAD_run-%d", run);
+            asprintf(&pdat.filename, "%s-%s_run-%d", pdat.title, mop->name, run);
             mgn_plot((mgn_pop_proto *) EP, &pdat);
         }
     }
