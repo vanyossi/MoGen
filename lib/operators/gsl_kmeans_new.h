4c9ec0ec0108156853a04386f60f049fed56f790
Ivan Yossi <ghevan@gmail.com>
2022-08-12 18:54:15 -0500

    Minor naming convention change

--- lib/operators/gsl_kmeans_new.h
+++ lib/operators/gsl_kmeans_new.h
@@ -24,7 +24,7 @@ struct mgn_kmeans_data {
 
 // after kmeans calc, this can be used to
 // analyze the data members.
-// TODO contain the points themselves as matrix
+// TODO contain the m_points themselves as matrix
 struct mgn_kmeans_data_idx {
     size_t size;
     unsigned int *pos;
